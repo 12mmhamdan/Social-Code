23e0dae8cb136541491efe2bcd300a772d9b3d94
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-10-05 15:48:50 -0500

    added user login/authentication on server, added assets folder

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "module";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -41,6 +41,9 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 
 app.post("/auth/register", upload.single("picture", register));
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 
 /* MONGOOSE SETUP */
