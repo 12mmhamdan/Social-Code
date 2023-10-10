6fcf9320142b132b527b0ff2d6b3cd319bf11753
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-10-10 13:37:06 -0500

    set up login page index.js

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          SocialCode
+          Social Code
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
