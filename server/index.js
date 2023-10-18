67d27abb9777b888a32650e6e089e480a479adec
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-10-18 18:34:54 -0500

    fixed index.js server side

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import path, { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
+import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
@@ -15,16 +16,15 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import cloudinary from 'cloudinary';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -32,31 +32,31 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* CLOUDINARY CONFIGURATION */
-cloudinary.v2.config({
-  cloud_name: 'djvpgim4w',
-  api_key: '238442638351457',
-  api_secret: 'xqr6JiZ_3Tb5umLBSa4hIayJQ0k'
+cloudinary.config({
+    cloud_name: 'djvpgim4w',
+    api_key: '238442638351457',
+    api_secret: 'xqr6JiZ_3Tb5umLBSa4hIayJQ0k'
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'uploads',
-    public_id: (req, file) => {
-      const uniquePrefix = Date.now().toString();
-      const randomString = Math.random().toString(36).substring(2, 7);
-      return `${uniquePrefix}_${randomString}`;
+    cloudinary: cloudinary,
+    params: {
+        folder: 'uploads',
+        format: (req, file) => file.originalname.split('.').pop(), // Use the original file extension
+        public_id: (req, file) => {
+            const uniquePrefix = Date.now().toString();
+            const randomString = Math.random().toString(36).substring(2, 7);
+            return `${uniquePrefix}_${randomString}`;
+        },
     },
-  },
 });
+
 
 const upload = multer({ storage: storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts/picture", verifyToken, upload.single("picture"), createPost);
-app.post("/posts/clip", verifyToken, upload.single("clip"), createPost);
-app.post("/posts/audio", verifyToken, upload.single("audio"), createPost);
+app.post("/posts", verifyToken, upload.fields([{ name: 'picture', maxCount: 1 }, { name: 'clip', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
@@ -66,8 +66,8 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
-  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`));
