bf04b220e63267a4fad9be94e574480207ccdfca
Moataz Hamdan <hamdanmoataz@gmail.com>
2024-08-04 20:02:41 -0500

    testing api connection

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -37,13 +37,14 @@ cloudinary.config({
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
 });
-console.log(cloudinary.config());
+
+console.log('Cloudinary Config:', cloudinary.config()); // Log the Cloudinary config
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'uploads',
-        format: (req, file) => file.originalname.split('.').pop(), // Use the original file extension
+        format: async (req, file) => file.originalname.split('.').pop(), // Use the original file extension
         public_id: (req, file) => {
             const uniquePrefix = Date.now().toString();
             const randomString = Math.random().toString(36).substring(2, 7);
@@ -52,17 +53,27 @@ const storage = new CloudinaryStorage({
     },
 });
 
-
 const upload = multer({ storage: storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.fields([{ name: 'picture', maxCount: 1 }, { name: 'clip', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), createPost);
+app.post("/auth/register", upload.single("picture"), (req, res, next) => {
+    register(req, res).catch(next);
+});
+
+app.post("/posts", verifyToken, upload.fields([{ name: 'picture', maxCount: 1 }, { name: 'clip', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), (req, res, next) => {
+    createPost(req, res).catch(next);
+});
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+
+/* ERROR HANDLING MIDDLEWARE */
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
